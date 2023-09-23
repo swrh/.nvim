035acbf7aa91c30c2d2abac51a37bc665cf66dae
Fernando Silveira <fsilveira@gmail.com>
2023-09-23 18:45:47 -0300

    Color scheme: onedark => tokyonight

--- init.lua
+++ init.lua
@@ -143,11 +143,10 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme('tokyonight-night')
     end,
   },
 
@@ -158,7 +157,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
