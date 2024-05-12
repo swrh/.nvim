33337f68f9b936d7aa1ce81200b4f000ba37986e
Fernando Silveira <fsilveira@gmail.com>
2024-05-12 12:27:48 -0300

    Add `terraformls` to the list of LSP servers

--- init.lua
+++ init.lua
@@ -567,6 +567,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
+        terraformls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
