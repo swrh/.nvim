97b1e1df9b4e40c491ce3417121929196fdd8922
Fernando Silveira <fsilveira@gmail.com>
2024-05-23 04:53:58 -0300

    Remove duplicate/conflicting mapping

--- init.lua
+++ init.lua
@@ -628,16 +628,6 @@ require('lazy').setup({
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_fallback = true }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
     opts = {
       notify_on_error = false,
       formatters_by_ft = {
