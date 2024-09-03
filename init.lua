08a2d674474fa85c8f3b0e40ce82711ebe75b461
Fernando Silveira <fsilveira@gmail.com>
2024-09-03 10:47:45 -0300

    Add `sqlls` to the list of LSP servers

--- init.lua
+++ init.lua
@@ -568,6 +568,7 @@ require('lazy').setup({
         clangd = {},
         gopls = {},
         terraformls = {},
+        sqlls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
